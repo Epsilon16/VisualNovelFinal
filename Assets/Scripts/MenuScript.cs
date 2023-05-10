cf9dcf62474819ca1f0abbb5b799f22251cc0795
Epsilon16 <marc.nebout@e-artsup.net>
2023-05-10 11:43:06 +0200

    Travail réalisé : Vrai interface de VN Menu sommaire

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(int GameScene)
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+
+    public void LoadGame(int GameScene)
+    {
+        Debug.Log(GameScene);
+    }
+
+    public void CGScene(int GameScene)
+    {
+        Debug.Log(GameScene);
+    }
+
+    public void OptionMenu(int GameScene)
+    {
+        Debug.Log(GameScene);
+    }
+
+    public void QuitGame(int GameScene)
+    {
+        Application.Quit();
+    }
+}
